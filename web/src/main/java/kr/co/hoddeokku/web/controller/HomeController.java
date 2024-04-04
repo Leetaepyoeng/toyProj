91c485751a80cec73d9484083167b3d672eb076f
Leetaepyoeng <48676338+Leetaepyoeng@users.noreply.github.com>
2024-04-04 23:43:41 +0900

    ver. 1.1.2 / 세션의 현재 사용자 정보 조회 추가

--- web/src/main/java/kr/co/hoddeokku/web/controller/HomeController.java
+++ web/src/main/java/kr/co/hoddeokku/web/controller/HomeController.java
@@ -1,20 +1,21 @@
 package kr.co.hoddeokku.web.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.hoddeokku.web.entity.Drink;
 import kr.co.hoddeokku.web.entity.Hodduk;
 import kr.co.hoddeokku.web.entity.Notice;
-import kr.co.hoddeokku.web.entity.User;
-import kr.co.hoddeokku.web.repository.UserRepository;
 import kr.co.hoddeokku.web.service.DrinkServiceImp;
 import kr.co.hoddeokku.web.service.HoddukServiceImp;
 import kr.co.hoddeokku.web.service.NoticeServiceImp;
@@ -42,6 +43,23 @@ public class HomeController {
         List<Drink> listD = serviceDrink.getList();
         List<Hodduk> listH = serviceHodd.getList();
         List<Notice> listN = serviceNot.getList();
+
+        //세션 현재 사용자 아이디 정보를 가져옴
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        //세션 현재 사용자 권한 정보를 가져옴
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+        
+        System.out.println(id);
+        System.out.println(role);
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
+
 
 
         model.addAttribute("listD", listD);
